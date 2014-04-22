157826ad248126c6ba90ff1d85b6744e98adf217
Vasyl Germanskij <rmacciawelli@mail.ru>
2014-04-22 13:47:08 +0300

    it You should Create Controller Pages with views index about info contactgit branch! do

--- spec/requests/main_tests_spec.rb
+++ spec/requests/main_tests_spec.rb
@@ -1,3 +1,20 @@
 require 'spec_helper'
 
 
+# тест 1 створення  контролера Pages із представленнями index about info contact
+describe "Rails Structure" do
+
+  describe "Controller creating" do
+    it 'You should Create Controller Pages with views index about info contact!!!' do
+    	expect(PagesController).to be
+    	get 'pages/about'
+    	response.should be_success
+     	get 'pages/index'
+     	response.should be_success
+    	get 'pages/contact'
+    	response.should be_success
+        get 'pages/info'
+        response.should be_success
+    end
+  end
+end
