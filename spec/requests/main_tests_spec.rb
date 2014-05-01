cb726dcb8a8e8ba5d0eee323e7daedcb11769100
Vasyl Germanskij <rmacciawelli@mail.ru>
2014-05-01 21:44:20 +0300

    it  You should add format validator for field email

--- spec/requests/main_tests_spec.rb
+++ spec/requests/main_tests_spec.rb
@@ -167,3 +167,27 @@ describe "Model" do
         end
     end
 end
+# тест15 Створення валідаторів формату поля email
+
+describe "Model" do
+
+  before { @user = User.new(name: "Example User", email: "user@example.com")}
+  
+  describe "Validators" do
+
+    it " You should add format validator for field email " do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+        end
+
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
+    end
+  end
+end
